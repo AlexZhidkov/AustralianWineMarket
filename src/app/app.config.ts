b1d81ad5f411a57005c780c7d79dc53a0ba8b41c
Alexander Zhidkov <azhidkov@gmail.com>
2024-01-29 16:53:46 +0800

    add angular fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,37 @@
-import { ApplicationConfig, isDevMode } from '@angular/core';
+import { ApplicationConfig, isDevMode, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
-    }), provideAnimations()]
+  providers: [
+    provideRouter(routes),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    provideAnimations(),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp({
+      "projectId": "australianwinemarket",
+      "appId": "1:885135086920:web:ca8358feabacd88c480f7d",
+      "storageBucket": "australianwinemarket.appspot.com",
+      "apiKey": "AIzaSyDtodPLhF2UyggaPTZiRzLGvBHpu659c-o",
+      "authDomain": "australianwinemarket.firebaseapp.com",
+      "messagingSenderId": "885135086920",
+      "measurementId": "G-94NM6T17P1"
+    }))),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideAnalytics(() => getAnalytics())),
+    ScreenTrackingService,
+    UserTrackingService,
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideStorage(() => getStorage()))
+  ]
 };
